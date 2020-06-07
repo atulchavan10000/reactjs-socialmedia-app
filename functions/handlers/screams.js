705becf8831f37d16f17ddfab4103f8b6aee0ee6
Atul <atulchavan528@gmail.com>
2020-06-07 23:02:08 +0530

    added like and comment count into the response for create scream api

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -30,14 +30,19 @@ exports.postOneScream = (req, res) => {
     const newScream = {
         body: req.body.body,
         userHandle: req.user.handle,
-        createdAt: new Date().toISOString()
+        userImage: req.user.imageUrl,
+        createdAt: new Date().toISOString(),
+        likeCount: 0,
+        commentCount: 0
     };
 
     db
         .collection('screams')
         .add(newScream)
         .then((doc) => {
-            res.json({ message: `document ${doc.id} created successfully` })
+            const resScream = newScream;
+            resScream.screamId = doc.id
+            res.json(resScream)
         })
         .catch(err => {
             res.status(500).json({ error: 'Something Went Wrong' });
