e93134989f1ba28e1d4c3e1ebf467a3268336c68
Atul <atulchavan528@gmail.com>
2020-06-07 18:28:15 +0530

    new route - get user details containing credentials, likes

--- functions/index.js
+++ functions/index.js
@@ -4,17 +4,25 @@ const FBAuth = require('./util/FBAuth');
 
 
 const { getAllScreams, postOneScream } = require('./handlers/screams');
-const { signup, login, uploadImage, addUserDetails} = require('./handlers/users');
+const { 
+    signup, 
+    login, 
+    uploadImage, 
+    addUserDetails,
+    getAuthenticatedUser
+} = require('./handlers/users');
 
 
 // Scream Routes
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
+app.post('/user/image', FBAuth, uploadImage);
+app.post('/user', FBAuth, addUserDetails);
+app.get('/user', FBAuth, getAuthenticatedUser);
 
 // users routes
 app.post('/signup', signup);
 app.post('/login', login);
-app.post('/user/image', FBAuth, uploadImage);
-app.post('/user', FBAuth, addUserDetails);
+
 
 exports.api = functions.region('asia-east2').https.onRequest(app);
