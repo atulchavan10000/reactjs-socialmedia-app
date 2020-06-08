7e5cc4fc73cda73629e20c6abb4d730ce31f3fb1
Atul <atulchavan528@gmail.com>
2020-06-08 11:34:20 +0530

    new route for deleteScream

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -195,3 +195,28 @@ screamDocument.get()
         res.status(500).json({ error: err.code })
     });
 }
+
+// Delete a scream
+exports.deleteScream = (req, res) => {
+    const document = db.doc(`/screams/${req.params.screamId}`);
+    document.get()
+        .then(doc => {
+            if(!doc.exists){
+                return res.status(404).json({ error: 'Scream not found'});
+            }
+            // check if the author of the scream and user trying the delete a scream
+            // is the same
+            if(doc.data().userHandle !== req.user.handle) {
+                return res.status(403).json({ error: 'Unauthorized'});
+            }else{
+                return document.delete();
+            }
+        })
+        .then(() => {
+            res.json({ message: 'Scream deleted successfully'});
+        })
+        .catch(err => {
+            console.err(err);
+            return res.status(500).json({ error: err.code });
+        })
+}
